a909035689e21596442dac726b976b1c98550123
Guilherme Martins <guilhermemartins.bg009@academico.ifsul.edu.br>
2026-05-12 19:30:09 -0300

    bancologin

--- banco.c
+++ banco.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
-#define senha Brasil2026
+#define Senha 2026
+int login()
+{
+    char usuario[50];
+    int senha;
+
+    printf("\nOlá bem-vindo ao banco Rotom, por favor insira o usuario e a senha: \n");
+    fgets(usuario, 50, stdin);
+    scanf("%d", &senha);
+
+    if (senha == Senha)
+    {
+        printf("Bem vindo %s", usuario);
+    }
+
+    else
+    {
+        printf("Senha incorreta! Por favor, tente novamente mais tarde.\n");
+        return 0;
+    }
+}
 int main(void)
 {
+
+    login();
+
+    return 0;
 }
